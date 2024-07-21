b906e0111b23d7981449006b3737ee359809972f
Petro <petrosota18@gmail.com>
2024-07-21 21:37:39 +0200

    Finitoo

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,27 +1,38 @@
 import { Card, Col, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { romance, fantasy, horror, scifi, history } from "../data/romance.js";
 import CommentArea from "./CommentArea";
-import { romance } from "../data/romance";
 
 const BookDetails = () => {
-    const params = useParams();
-    const foundBook = romance.find((book) => book.asin === params.asin);
+  const { asin } = useParams();
+  const [book, setBook] = useState(null);
+
+  useEffect(() => {
+    const allBooks = [...romance, ...fantasy, ...horror, ...scifi, ...history];
+    const selectedBook = allBooks.find((b) => b.asin === asin);
+    setBook(selectedBook);
+  }, [asin]);
+
+  if (!book) {
+      return <div>Book not found</div>;
+    }
 
     return (
         <Row className="justify-content-center">
           <Col md={8}>
             <Card>
-              <Card.Img variant="top" src={foundBook.img} className="object-fit-cover" />
+              <Card.Img variant="top" src={book.img} className="object-fit-cover" />
               <Card.Body>
                 <Card.Title style={{ color: 'black' }}>
-                  {foundBook.title}
+                  {book.title}
                 </Card.Title>
                 <Card.Text className="text-center">
-                    {foundBook.price} €
+                    {book.price} €
                 </Card.Text>
               </Card.Body>
             </Card>
-            <CommentArea asin={params.asin} />
+            <CommentArea asin={asin} />
           </Col>
         </Row>
       )
